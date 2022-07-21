1558009905ef3cbf1e66283de6284f5da1610a79
panchock <amosran96@gmail.com>
2022-07-21 23:01:37 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mindstorms-api",
+    version="0.0.1",
+    author="panchock",
+    description="Api without implementation for Robot Inventor mindstorms python library",
+    packages=["mindstorms"]
+)
